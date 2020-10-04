5e287e419f6241b966de23732e724c1a848a319f
Richard Otto <richardsoeuzin@gmail.com>
2020-10-04 20:11:49 -0300

    feat: added custom text selection style

--- src/App.vue
+++ src/App.vue
@@ -16,11 +16,19 @@ export default {
 </script>
 
 
-<style lang="sass" scoped>
+<style lang="sass">
 #app
   display: flex
   box-sizing: border-box
   background: lightblue
   min-height: 100vh
   padding: .8em
+
+::selection
+  background: #41ff33
+  color: #1b1b1b
+
+::-moz-selection
+  background: #41ff33
+  color: #1b1b1b
 </style>
